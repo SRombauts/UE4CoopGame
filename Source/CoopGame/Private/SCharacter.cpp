93849da355301e22f576f8f70af63510cc81a681
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2018-03-06 08:45:29 +0100

    Add our own Character class with basic translation movements

---
+++ Source/CoopGame/Private/SCharacter.cpp
@@ -0,0 +1,46 @@
+// Copyright 2018 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+
+#include "SCharacter.h"
+
+
+// Sets default values
+ASCharacter::ASCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector() * Value);
+}
+
+void ASCharacter::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector() * Value);
+}
+
+// Called every frame
+void ASCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASCharacter::MoveRight);
+}
+
---
+++ Source/CoopGame/Public/SCharacter.h
@@ -0,0 +1,32 @@
+// Copyright 2018 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SCharacter.generated.h"
+
+UCLASS()
+class COOPGAME_API ASCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+};
