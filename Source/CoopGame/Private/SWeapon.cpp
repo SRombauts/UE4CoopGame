34f448da34f5485012ebd9ea42d549a387962f40
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2018-03-09 21:46:50 +0100

    Add a LineTrace with DrawDebugLine on Hit

--- Source/CoopGame/Private/SWeapon.cpp
+++ Source/CoopGame/Private/SWeapon.cpp
@@ -3,12 +3,41 @@
 #include "SWeapon.h"
 
 #include "Components/SkeletalMeshComponent.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 ASWeapon::ASWeapon()
 {
 	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
 	RootComponent = SkeletalMeshComponent;
+}
+
+void ASWeapon::Fire()
+{
+	UWorld* World = GetWorld();
+	AActor* Owner = GetOwner();
+	if (World && Owner)
+	{
+		// Trace the world from pawn eyes toward crosshair direction
+		FVector TraceStart;
+		FRotator ViewpointOrientation;
+		Owner->GetActorEyesViewPoint(TraceStart, ViewpointOrientation);
+		const FVector TraceEnd = TraceStart + ViewpointOrientation.Vector() * 10000.f;
+
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(Owner);
+		QueryParams.AddIgnoredActor(this);
+		QueryParams.bTraceComplex = true;
+
+		FHitResult HitResult;
+		const bool bHit = World->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ECollisionChannel::ECC_Visibility, QueryParams);
+		if (bHit)
+		{
+			DrawDebugLine(GetWorld(), TraceStart, TraceEnd, FColor::Green, false, 1.f, 0, 1.f);
+
+			// TODO: blocking hit, process damages
+		}
+	}
 }
 
 // Called when the game starts or when spawned
--- Source/CoopGame/Public/SWeapon.h
+++ Source/CoopGame/Public/SWeapon.h
@@ -15,6 +15,9 @@ public:
 	// Sets default values for this actor's properties
 	ASWeapon();
 
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void Fire();
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
